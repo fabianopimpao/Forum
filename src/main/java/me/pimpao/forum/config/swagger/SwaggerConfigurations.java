5611467909763dcf448a908b53980585fef281e4
Fabiano Pimpão <fabiano.pimpao@gmail.com>
2020-10-13 07:17:33 -0300

    Sending the Jwt Token in the Swagger

--- src/main/java/me/pimpao/forum/config/swagger/SwaggerConfigurations.java
+++ src/main/java/me/pimpao/forum/config/swagger/SwaggerConfigurations.java
@@ -3,10 +3,14 @@ package me.pimpao.forum.config.swagger;
 import me.pimpao.forum.model.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
 
 @Configuration
 public class SwaggerConfigurations {
@@ -18,6 +22,14 @@ public class SwaggerConfigurations {
                 .apis(RequestHandlerSelectors.basePackage("me.pimpao.forum"))
                 .paths(PathSelectors.ant("/**"))
                 .build()
-                .ignoredParameterTypes(User.class);
+                .ignoredParameterTypes(User.class)
+                .globalOperationParameters(Arrays.asList(
+                        new ParameterBuilder()
+                        .name("Authorization")
+                        .description("Header para token JWT")
+                        .modelRef(new ModelRef("string"))
+                        .parameterType("header")
+                        .required(false)
+                        .build()));
     }
 }
