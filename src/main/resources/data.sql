87dc729ba160257456d181b268c6e34e74e961e8
Fabiano Pimpão <fabiano.pimpao@gmail.com>
2020-10-16 07:02:27 -0300

    Role-based authorization

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,11 @@
 INSERT INTO USER(name, email, password) VALUES('Aluno', 'aluno@email.com', '$2a$10$6JTvLJDuVGVRbJd/iSY7w.92m8ir2rc6hpBxH8OWBE7CffyixqHhS');
+INSERT INTO USER(name, email, password) VALUES('Moderador', 'moderador@email.com', '$2a$10$6JTvLJDuVGVRbJd/iSY7w.92m8ir2rc6hpBxH8OWBE7CffyixqHhS');
+
+INSERT INTO PROFILE(id, name) VALUES(1, 'ROLE_STUDENT');
+INSERT INTO PROFILE(id, name) VALUES(2, 'ROLE_MODERATOR');
+
+INSERT INTO USER_PROFILES(user_id, profiles_id) VALUES(1, 1);
+INSERT INTO USER_PROFILES(user_id, profiles_id) VALUES(2, 2);
 
 INSERT INTO COURSE(name, category) VALUES('Spring Boot', 'Programação');
 INSERT INTO COURSE(name, category) VALUES('HTML 5', 'Front-end');
