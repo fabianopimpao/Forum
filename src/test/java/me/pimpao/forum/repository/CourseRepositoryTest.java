2a21f1c4eaa5fd073604eec468796c16ff21cee6
Fabiano Pimpão <fabiano.pimpao@gmail.com>
2020-10-20 07:19:14 -0300

    Testing the repository with another database

--- src/test/java/me/pimpao/forum/repository/CourseRepositoryTest.java
+++ src/test/java/me/pimpao/forum/repository/CourseRepositoryTest.java
@@ -4,17 +4,31 @@ import me.pimpao.forum.model.Course;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 public class CourseRepositoryTest {
 
     @Autowired
     private CourseRepository courseRepository;
 
+    @Autowired
+    private TestEntityManager em;
+
     @Test
     public void shouldLoadACourseWhenSearchingForYourName() {
         String courseName = "HTML 5";
+
+        Course html5 = new Course();
+        html5.setName(courseName);
+        html5.setCategory("Programação");
+        em.persist(html5);
+
         Course course = courseRepository.findByName(courseName);
         Assert.assertNotNull(course);
         Assert.assertEquals(courseName, course.getName());
